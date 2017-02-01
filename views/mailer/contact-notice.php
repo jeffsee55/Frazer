7dc132a23c86d5b3e2177eeda7f11069a872a839
Jeff See <jeffsee.55@gmail.com>
2017-01-31 21:07:53 -0500

    Update gmail

--- views/mailer/contact-notice.php
+++ views/mailer/contact-notice.php
@@ -140,7 +140,7 @@
               <table border="0" cellpadding="0" cellspacing="0" style="border-collapse:separate;mso-table-lspace:0pt;mso-table-rspace:0pt;width:100%;">
                 <tr>
                   <td class="content-block" style="font-family:sans-serif;font-size:14px;vertical-align:top;color:#999999;font-size:12px;text-align:center;">
-                    <span class="apple-link" style="color:#999999;font-size:12px;text-align:center;">Frazer Finer Foods, 123 Street, Cincinnati, OH 12345</span>
+                    <span class="apple-link" style="color:#999999;font-size:12px;text-align:center;"><?= bloginfo('name') ?>, <?= the_field('fff_address', 'option') ?></span>
                   </td>
                 </tr>
                 <tr>
